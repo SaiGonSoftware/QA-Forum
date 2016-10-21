da41299d3145e58a11ce45d9524a342e894f03c5
hoangphucvu <ngohungphuc95@gmail.com>
2016-10-21 15:51:41 +0700

    Generate hashpassword

--- Source/app/controllers/login.server.controller.js
+++ Source/app/controllers/login.server.controller.js
@@ -1,10 +1,12 @@
 /*
 * @Author: hoangphucvu
 * @Date:   2016-10-20 13:19:53
-* @Last Modified by:   Ngo Hung Phuc
-* @Last Modified time: 2016-10-20 19:16:11
+* @Last Modified by:   hoangphucvu
+* @Last Modified time: 2016-10-21 15:50:34
 */
-
+var passwordHash = require('password-hash');
+var hashedPassword = passwordHash.generate('070695');
+var hashedPassword1 = passwordHash.generate('123456');
 var User = require('../models/user.server.model');
 exports.Index = function(req,res){
 	res.render('account/login',{title:'Đăng Nhập'});
@@ -13,6 +15,8 @@ exports.Index = function(req,res){
 exports.Login = function(req,res){
 	var username = req.body.userName;
 	var password = req.body.passWord;
+	console.log(hashedPassword);
+	console.log(hashedPassword1);
 	User.findOne({username:username,password:password},function(err,user){
 		if(err){
 			console.log(err);
