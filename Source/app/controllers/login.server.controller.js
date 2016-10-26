113267cd2fef22c56bf97d39020d132379ac9b48
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-10-26 22:07:53 +0700

    Todo fix password-hash

--- Source/app/controllers/login.server.controller.js
+++ Source/app/controllers/login.server.controller.js
@@ -1,12 +1,10 @@
 /*
 * @Author: hoangphucvu
 * @Date:   2016-10-20 13:19:53
-* @Last Modified by:   hoangphucvu
-* @Last Modified time: 2016-10-21 15:50:34
+* @Last Modified by:   Ngo Hung Phuc
+* @Last Modified time: 2016-10-26 21:50:37
 */
 var passwordHash = require('password-hash');
-var hashedPassword = passwordHash.generate('070695');
-var hashedPassword1 = passwordHash.generate('123456');
 var User = require('../models/user.server.model');
 exports.Index = function(req,res){
 	res.render('account/login',{title:'Đăng Nhập'});
@@ -15,9 +13,8 @@ exports.Index = function(req,res){
 exports.Login = function(req,res){
 	var username = req.body.userName;
 	var password = req.body.passWord;
-	console.log(hashedPassword);
-	console.log(hashedPassword1);
 	User.findOne({username:username,password:password},function(err,user){
+
 		if(err){
 			console.log(err);
 			return res.status(500).send();
