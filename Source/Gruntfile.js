fcf6f28a7dffa5b2e889493195c7cd95f9f9f49d
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-11-05 22:18:40 +0700

    Setup forum structure

--- Source/Gruntfile.js
+++ Source/Gruntfile.js
@@ -28,11 +28,33 @@ module.exports = function (grunt) {
             all: ['Gruntfile.js', 'public/frontend/**/!*.min.js']
         },
         nodemon: {
-          dev: {
-            script: 'app.js'
+            dev: {
+                script: 'app.js',
+                options: {
+                    ext: 'js,jade',
+                    // omit this property if you aren't serving HTML files and 
+                    // don't want to open a browser tab on start
+                    callback: function(nodemon) {
+                        // opens browser on initial server start
+                        nodemon.on('config:update', function() {
+                            // Delay before server listens on port
+                            setTimeout(function() {
+                                require('open')('http://localhost:3000');
+                            }, 1000);
+                        });
+
+                        // refreshes browser when server reboots
+                        nodemon.on('restart', function() {
+                            // Delay before server listens on port
+                            setTimeout(function() {
+                                require('fs').writeFileSync('.rebooted', 'rebooted');
+                            }, 1000);
+                        });
+                    }
+                }
+            },
         }
-    }
-});
+    });
 
     grunt.log.write('Grunt is running\n');
     grunt.registerTask('default',['jshint','cssmin','uglify','nodemon']);
--- Source/app/controllers/index.server.controller.js
+++ Source/app/controllers/index.server.controller.js
@@ -1,15 +1,16 @@
 /*
 * @Author: hoangphucvu
 * @Date:   2016-10-20 13:55:32
-* @Last Modified by:   hoangphucvu
-* @Last Modified time: 2016-10-20 14:11:53
+* @Last Modified by:   Ngo Hung Phuc
+* @Last Modified time: 2016-11-05 22:17:38
 */
 
 var User = require('../models/user.server.model');
 exports.Index = function(req,res){
-	if (!req.session.user) {
+	/*if (!req.session.user) {
 		res.redirect('/');
 	} else {
 		res.render('site/index', { title: 'Trang Chủ' });
-	}
+	}*/
+	res.render('site/index', { title: 'Trang Chủ' });
 };
