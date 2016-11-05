9433bbad692b2b45c1673346e833c615126624bd
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-11-05 22:47:45 +0700

    Details page for forum

--- Source/app/controllers/index.server.controller.js
+++ Source/app/controllers/index.server.controller.js
@@ -2,7 +2,7 @@
 * @Author: hoangphucvu
 * @Date:   2016-10-20 13:55:32
 * @Last Modified by:   Ngo Hung Phuc
-* @Last Modified time: 2016-11-05 22:17:38
+* @Last Modified time: 2016-11-05 22:43:33
 */
 
 var User = require('../models/user.server.model');
@@ -13,4 +13,12 @@ exports.Index = function(req,res){
 		res.render('site/index', { title: 'Trang Chủ' });
 	}*/
 	res.render('site/index', { title: 'Trang Chủ' });
+};
+
+exports.Detail = function(req,res){
+	res.render('site/details', { title: 'Thông Tin' });
+};
+
+exports.Login = function(req,res){
+	res.render('account/login', { title: 'Đăng Nhập' });
 };
--- Source/app/routes/index.server.routes.js
+++ Source/app/routes/index.server.routes.js
@@ -2,7 +2,7 @@
 * @Author: hoangphucvu
 * @Date:   2016-10-20 13:55:32
 * @Last Modified by:   Ngo Hung Phuc
-* @Last Modified time: 2016-10-29 21:33:52
+* @Last Modified time: 2016-11-05 22:43:08
 */
 
 var express = require('express');
@@ -10,5 +10,6 @@ var router = express.Router();
 var indexController = require('../controllers/index.server.controller');
 
 router.get('/',indexController.Index);
-
+router.get('/details',indexController.Detail);
+router.get('/dang-nhap',indexController.Login);
 module.exports = router;
