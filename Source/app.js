c3d575b945454d619fc9ad084f9718c585ee679c
hoangphucvu <ngohungphuc95@gmail.com>
2016-11-09 14:40:42 +0700

    Todo fix 404 error

--- Source/app.js
+++ Source/app.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var db = require('./config/db');
-var app = express();
-var port = process.env.PORT || 3000;
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    db = require('./config/db'),
+    app = express(),
+    port = process.env.PORT || 3000;
 
 //require route for app
-var loginRoute = require('./app/routes/login.server.routes');
-var indexRoute = require('./app/routes/index.server.routes');
+var loginRoute = require('./app/routes/login.server.routes'),
+    indexRoute = require('./app/routes/index.server.routes');
 
 //app.locals.pretty = true;
 // view engine setup
@@ -33,46 +33,12 @@ app.use(session({
     saveUninitialized: true
 }));
 
+
 //set route for specific request
-//app.use('/', loginRoute);
-app.use('/', indexRoute);
 app.get('/partials/:partialPath', function(req, res) {
     res.render('partials/' + req.params.partialPath);
 });
-/*app.get('*', function(req, res) {
-    res.render('site/index');
-});*/
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('layout/error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('layout/error', {
-        message: err.message,
-        error: {}
-    });
-});
+app.get('*', indexRoute);
 
 
 app.listen(port, function(req, res) {
