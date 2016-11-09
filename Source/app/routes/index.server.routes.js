a0cb372815d3d13a77251caf323c5f6a42ada3a3
hoangphucvu <ngohungphuc95@gmail.com>
2016-11-09 13:58:00 +0700

    Todo fix f5 error

--- Source/app/routes/index.server.routes.js
+++ Source/app/routes/index.server.routes.js
@@ -1,21 +1,15 @@
 /*
-* @Author: hoangphucvu
-* @Date:   2016-10-20 13:55:32
-* @Last Modified by:   hoangphucvu
-* @Last Modified time: 2016-11-07 09:02:36
-*/
+ * @Author: hoangphucvu
+ * @Date:   2016-10-20 13:55:32
+ * @Last Modified by:   hoangphucvu
+ * @Last Modified time: 2016-11-09 13:26:24
+ */
 
 var express = require('express');
 var router = express.Router();
 var indexController = require('../controllers/index.server.controller');
 
-router.get('/',indexController.Index);
-router.get('/partials/:name',indexController.Partial);
-router.get('/details',indexController.Detail);
-router.get('/dang-nhap',indexController.Login);
-/*
-router.get('/partials/:name',indexController.Partial);
-router.get('/details',indexController.Detail);
-router.get('/dang-nhap',indexController.Login);*/
+router.get('/', indexController.Index);
+
 
 module.exports = router;
