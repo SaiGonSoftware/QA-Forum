2ff43d829b9f81a81cd43e09c674ce8c8288b137
hoangphucvu <ngohungphuc95@gmail.com>
2016-11-09 11:33:51 +0700

    Auto update file after change

--- Source/app/controllers/index.server.controller.js
+++ Source/app/controllers/index.server.controller.js
@@ -1,30 +1,36 @@
 /*
-* @Author: hoangphucvu
-* @Date:   2016-10-20 13:55:32
-* @Last Modified by:   hoangphucvu
-* @Last Modified time: 2016-11-07 09:03:07
-*/
+ * @Author: hoangphucvu
+ * @Date:   2016-10-20 13:55:32
+ * @Last Modified by:   hoangphucvu
+ * @Last Modified time: 2016-11-09 10:06:54
+ */
 
 var User = require('../models/user.server.model');
-exports.Index = function(req,res){
-	/*if (!req.session.user) {
-		res.redirect('/');
-	} else {
-		res.render('site/index', { title: 'Trang Chủ' });
-	}*/
-	res.render('site/index', { title: 'Trang Chủ' });
+exports.Index = function(req, res) {
+    /*if (!req.session.user) {
+    	res.redirect('/');
+    } else {
+    	res.render('site/index', { title: 'Trang Chủ' });
+    }*/
+    res.render('site/index', {
+        title: 'Trang Chủ'
+    });
 };
 
-exports.Partial = function (req, res) {
-  var name = req.params.name;
-  console.log(name);
-  res.render('partials/view1');
+exports.Partial = function(req, res) {
+    var name = req.params.name;
+    console.log(name);
+    res.render('partials/' + name);
 };
 
-exports.Detail = function(req,res){
-	res.render('site/details', { title: 'Thông Tin' });
+exports.Detail = function(req, res) {
+    res.render('site/details', {
+        title: 'Thông Tin'
+    });
 };
 
-exports.Login = function(req,res){
-	res.render('account/login', { title: 'Đăng Nhập' });
+exports.Login = function(req, res) {
+    res.render('account/login', {
+        title: 'Đăng Nhập'
+    });
 };
