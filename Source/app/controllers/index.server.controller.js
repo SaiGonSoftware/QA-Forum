240a6e971424b1e0135f95ed7258b1ffa2235d1e
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-11-11 21:44:34 +0700

    Update code

--- Source/app/controllers/index.server.controller.js
+++ Source/app/controllers/index.server.controller.js
@@ -1,8 +1,8 @@
 /*
  * @Author: hoangphucvu
  * @Date:   2016-10-20 13:55:32
- * @Last Modified by:   hoangphucvu
- * @Last Modified time: 2016-11-11 07:24:35
+ * @Last Modified by:   Ngo Hung Phuc
+ * @Last Modified time: 2016-11-11 21:10:35
  */
 
 var User = require('../models/user.server.model');
@@ -13,7 +13,7 @@ exports.Index = function(req, res) {
     	res.render('site/index', { title: 'Trang Chủ' });
     }*/
     res.render('site/index', {
-        title: 'Trang Chủ'
+        title: 'HỆ THỐNG TƯ VẤN TRỰC TUYẾN'
     });
 };
 
@@ -23,20 +23,8 @@ exports.Partial = function(req, res) {
     res.render('partials/' + name);
 };
 
-exports.Detail = function(req, res) {
-    res.render('site/details', {
-        title: 'Thông Tin'
-    });
-};
-
-exports.Login = function(req, res) {
-    res.render('account/login', {
-        title: 'Đăng Nhập'
-    });
-};
-
 exports.All = function(req, res) {
     res.render('site/index', {
-        title: 'Trang Chủ'
+        title: 'HỆ THỐNG TƯ VẤN TRỰC TUYẾN'
     });
 };
