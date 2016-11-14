654760811ccdff2822251bd6986ebbfc43c8ff3d
hoangphucvu <ngohungphuc95@gmail.com>
2016-11-14 11:37:58 +0700

    Adding animation

--- Source/public/frontend/app.client.js
+++ Source/public/frontend/app.client.js
@@ -2,14 +2,14 @@
  * @Author: Ngo Hung Phuc
  * @Date:   2016-10-29 21:57:50
  * @Last Modified by:   hoangphucvu
- * @Last Modified time: 2016-11-14 10:19:45
+ * @Last Modified time: 2016-11-14 11:37:18
  */
 
 (function() {
     'use strict';
     angular.module('ChatBotApp', [
         // Angular modules
-
+        'ngAnimate',
         // Custom modules
         'appRoute'
         // 3rd Party Modules
