bb0ec7a9dfb242fe8095eac8d91e4e2902927e5d
hoangphucvu <ngohungphuc95@gmail.com>
2016-11-15 13:34:35 +0700

    404 handler

--- Source/public/frontend/config/routes.js
+++ Source/public/frontend/config/routes.js
@@ -1,11 +1,16 @@
 (function() {
     'use strict';
     angular.module('appRoute', ['ngRoute'])
-        .config(config);
+        .config(config)
+        .run(run);
     config.$inject = ['$locationProvider', '$routeProvider'];
+    run.$inject = ['$rootScope', '$location'];
 
     function config($locationProvider, $routeProvider) {
         $routeProvider
+            .when('/', {
+                templateUrl: '/partials/index'
+            })
             .when('/view1', {
                 templateUrl: '/partials/view1'
             })
@@ -15,12 +20,23 @@
             .when('/details', {
                 templateUrl: '/partials/details'
             })
+            .when('/details', {
+                templateUrl: '/partials/details'
+            })
             .otherwise({
-                templateUrl: '/partials/index'
+                templateUrl: '/partials/404'
             });
         $locationProvider.html5Mode({
             enabled: true,
             requireBase: false
+        });
+    }
+
+    function run($rootScope, $location) {
+        $rootScope.$on('$routeChangeError', function(event, current, previous, error) {
+            console.log(error);
+            if (error.status === 404)
+                $location.path('/not-found');
         });
     }
 })();
