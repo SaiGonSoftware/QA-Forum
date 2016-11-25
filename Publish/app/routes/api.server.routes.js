92bac805563204b43e801c75f2b933ed7ed1508b
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-11-26 01:35:50 +0700

    Todo get data in angular

--- Publish/app/routes/api.server.routes.js
+++ Publish/app/routes/api.server.routes.js
@@ -2,10 +2,11 @@
  * @Author: Ngo Hung Phuc
  * @Date:   2016-11-18 20:05:10
  * @Last Modified by:   Ngo Hung Phuc
- * @Last Modified time: 2016-11-18 20:20:07
+ * @Last Modified time: 2016-11-26 00:10:11
  */
 var express = require('express');
 var router = express.Router();
 var api = require('../controllers/api.server.controller');
 router.get('/GetAllQuestion', api.QuestionIndex);
+router.get('/GetQuestionDetail/:id', api.QuestionDetail);
 module.exports = router;
