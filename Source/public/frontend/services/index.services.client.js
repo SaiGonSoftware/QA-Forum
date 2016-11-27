6e1ed109860b842b7e48453a51c99d43b327714b
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-11-27 22:29:07 +0700

    Todo pagination

--- Source/public/frontend/services/index.services.client.js
+++ Source/public/frontend/services/index.services.client.js
@@ -2,7 +2,7 @@
  * @Author: hoangphucvu
  * @Date:   2016-11-18 14:10:38
  * @Last Modified by:   Ngo Hung Phuc
- * @Last Modified time: 2016-11-18 21:17:12
+ * @Last Modified time: 2016-11-27 22:21:07
  */
 (function() {
     'use strict';
