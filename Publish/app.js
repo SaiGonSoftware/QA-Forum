2386d1adb5872c75c96b5ad9c131fd47e9c349e2
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-12-14 22:33:06 +0700

    Fb login Todo fix set session and fix load data using fb

--- Publish/app.js
+++ Publish/app.js
@@ -10,27 +10,30 @@ var express = require('express'),
     port = process.env.PORT || 3000,
     http = require('http'),
     server = http.createServer(app),
-    env = process.env.NODE_ENV || 'production',
-    passport = require('passport'),
-    localStrategy = require('passport-local').Strategy;
+    env = process.env.NODE_ENV || 'development';
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(cookieParser());
-// view engine setup
-app.set('views', path.join(__dirname, 'app/views'));
-app.set('view engine', 'jade');
-//resave update session avoid it from expires
-app.use(session({
-    secret: "asdkoasdkascmkascpoascmkalscasoi",
-    resave: true,
-    saveUninitialized: true
-}));
+    app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
+    app.use(cookieParser());
+    //resave update session avoid it from expires
+    app.use(session({
+        secret: "asdkoasdkascmkascpoascmkalscasoi",
+        resave: true,
+        saveUninitialized: true,
+        duration: 30 * 60 * 1000,
+        activeDuration: 5 * 60 * 1000
+    }));
+
+    // view engine setup
+    app.set('views', path.join(__dirname, 'app/views'));
+    app.set('view engine', 'jade');
+
+
 //require route for app
 var indexRoute = require('./app/routes/index.server.routes'),
     apiRoute = require('./app/routes/api.server.routes');
--- Source/public/frontend/controllers/facebook.client.controller.js
+++ Source/public/frontend/controllers/facebook.client.controller.js
@@ -1,3 +1,70 @@
 /**
  * Created by ngohungphuc on 14/12/2016.
  */
+/*
+ * @Author: Ngo Hung Phuc
+ * @Date:   2016-11-06 22:10:19
+ * @Last Modified by:   Ngo Hung Phuc
+ * @Last Modified time: 2016-11-19 22:37:18
+ */
+(function () {
+    'use strict';
+
+    angular.module('ChatBotApp')
+        .controller('FacebookController', FacebookController);
+    FacebookController.$inject = ['$scope'];
+
+    function FacebookController($scope) {
+        $scope.isFacebookLogin = false;
+        $scope.facebookLogin = function () {
+            FB.login(function (response) {
+                if (response.authResponse) {
+                    FB.api('/me', function (response) {
+                        $scope.isFacebookLogin = true;
+                        $scope.welcomeMsg = 'Xin chào ' + response.name;
+                        FB.api(response.id + '/picture', function (response) {
+                            $scope.profileImg = response.data.url;
+                            refresh();
+                        });
+                    });
+                }
+            });
+
+        };
+
+        $scope.facebookLogout = function () {
+
+        };
+
+        function refresh() {
+            FB.api('/me', function (response) {
+                $scope.isFacebookLogin = true;
+                $scope.welcomeMsg = 'Xin chào ' + response.name;
+                FB.api(response.id + '/picture', function (response) {
+                    $scope.profileImg = response.data.url;
+                });
+            });
+        }
+    }
+
+
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: '1211287998940961',
+            xfbml: true,
+            version: 'v2.8'
+        });
+    };
+
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+})();
