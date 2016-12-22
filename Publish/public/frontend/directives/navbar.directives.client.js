d803064c2d65e1da085fbc52853f0e24ab3a20cc
hoangphucvu <ngohungphuc95@gmail.com>
2016-12-22 09:23:08 +0700

    Upade code todo fix navbar directive

---
+++ Publish/public/frontend/directives/navbar.directives.client.js
@@ -0,0 +1,29 @@
+/**
+ * Created by phuc.ngo on 12/20/2016.
+ */
+/*(function () {
+    'use strict';
+    angular
+        .module('ChatBotApp')
+        .directive('navbar', navbar);
+
+    navbar.$inject = ['localStorageService'];
+    function navbar(localStorageService) {
+        return {
+            restrict: 'E',
+            templateUrl: 'navbar.jade',
+            scope: true,
+            controller: function ($scope) {
+                var loginUser = localStorageService.get('currentUser');
+                if (loginUser) {
+                    $scope.HideLoginSection = true;
+                    $scope.IsLogin = true;
+                }
+                else {
+                    $scope.HideLoginSection = false;
+                    $scope.IsLogin = false;
+                }
+            }
+        };
+    }
+})();*/
