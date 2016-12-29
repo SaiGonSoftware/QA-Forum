b34a69b582bf8327e11df3b661ca52402598fa2d
hoangphucvu <ngohungphuc95@gmail.com>
2016-12-29 15:21:30 +0700

    Update answer model

--- Source/app/models/answer.server.model.js
+++ Source/app/models/answer.server.model.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Ngo Hung Phuc
  * @Date:   2016-11-19 15:38:22
- * @Last Modified by:   Ngo Hung Phuc
- * @Last Modified time: 2016-11-26 01:19:28
+ * @Last Modified by:   hoangphucvu
+ * @Last Modified time: 2016-12-29 15:21:21
  */
 
 var mongoose = require('mongoose');
@@ -24,42 +24,74 @@ var answerSchema = new mongoose.Schema({
         type: Date,
         require: true
     },
-    like: {
+    References: {
         type: Array,
         default: []
     },
-    dislike: {
+    Like: {
+        type: Array,
+        default: []
+    },
+    Dislike: {
         type: Array,
         default: []
     }
 });
 
 var Answer = mongoose.model('answers', answerSchema);
-var getAnswerViaQuestion = function (id, callback) {
-    Answer.find({"QuestionId": {"$in": id}}, callback);
+var getAnswerViaQuestion = function(id, callback) {
+    Answer.find({
+        "QuestionId": {
+            "$in": id
+        }
+    }, callback);
 };
-var submitAnswer = function (answer, callback) {
+var submitAnswer = function(answer, callback) {
     Answer.collection.insert(answer, callback);
 };
 
-var addLike = function(answerId,username, callback){
-    Answer.collection.update({_id:ObjectId(answerId)},{ "$push": { "like": username} },callback);
+var addLike = function(answerId, username, callback) {
+    Answer.collection.update({
+        _id: ObjectId(answerId)
+    }, {
+        "$push": {
+            "like": username
+        }
+    }, callback);
 };
-var unLike = function(answerId,username, callback){
-    Answer.collection.update({_id:ObjectId(answerId)},{ "$pull": { "like": username} },callback);
+var unLike = function(answerId, username, callback) {
+    Answer.collection.update({
+        _id: ObjectId(answerId)
+    }, {
+        "$pull": {
+            "like": username
+        }
+    }, callback);
 };
-var addDislike = function(answerId,username, callback){
-    Answer.collection.update({_id:ObjectId(answerId)},{ "$push": { "dislike": username} },callback);
+var addDislike = function(answerId, username, callback) {
+    Answer.collection.update({
+        _id: ObjectId(answerId)
+    }, {
+        "$push": {
+            "dislike": username
+        }
+    }, callback);
 };
-var unDislike = function(answerId,username, callback){
-    Answer.collection.update({_id:ObjectId(answerId)},{ "$pull": { "dislike": username} },callback);
+var unDislike = function(answerId, username, callback) {
+    Answer.collection.update({
+        _id: ObjectId(answerId)
+    }, {
+        "$pull": {
+            "dislike": username
+        }
+    }, callback);
 };
 module.exports = {
     Answer: Answer,
     getAnswerViaQuestion: getAnswerViaQuestion,
     submitAnswer: submitAnswer,
-    addLike:addLike,
-    unLike:unLike,
-    addDislike:addDislike,
+    addLike: addLike,
+    unLike: unLike,
+    addDislike: addDislike,
     unDislike: unDislike
 };
