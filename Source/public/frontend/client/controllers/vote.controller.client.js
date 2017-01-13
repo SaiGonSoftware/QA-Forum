ad5ac7d72f30bd48eacdc60738497dbd4dcf25db
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-01-13 22:35:08 +0700

    Fix fb like

--- Source/public/frontend/client/controllers/vote.controller.client.js
+++ Source/public/frontend/client/controllers/vote.controller.client.js
@@ -11,17 +11,18 @@
     LikeController.$inject = ['$scope', 'localStorageService', 'LikeService'];
     DislikeController.$inject = ['$scope', 'localStorageService', 'DislikeService'];
     function LikeController($scope, localStorageService, LikeService) {
+        var loginUser = localStorageService.cookie.get('currentUser');
+        var facebookUser = localStorageService.cookie.get('facebookUser');
         $scope.LikeAnswer = function (answerId) {
             $scope.LikeData = {
                 UserLike: localStorageService.cookie.get('currentUser'),
                 AnswerId: answerId
             };
-            if (localStorageService.cookie.get('currentUser') === null) {
+            if (!loginUser || !facebookUser) {
                 toastr.warning("Vui lòng đăng nhập để like bài viết");
                 return false;
             }
             LikeService.LikeAnswer($scope.LikeData).then(function (result) {
-                console.log(result);
                 if (result.data.checkLikeAndDislike) {
                     $("#like-" + answerId).text(result.data.totalLike);
                     $("#dislike-" + answerId).text(result.data.totalDislike);
@@ -35,17 +36,18 @@
     }
 
     function DislikeController($scope, localStorageService, DislikeService) {
+        var loginUser = localStorageService.cookie.get('currentUser');
+        var facebookUser = localStorageService.cookie.get('facebookUser');
         $scope.DislikeAnswer = function (answerId) {
             $scope.DislikeData = {
                 UserDislike: localStorageService.cookie.get('currentUser'),
                 AnswerId: answerId
             };
-            if (localStorageService.cookie.get('currentUser') === null) {
+            if (!loginUser || !facebookUser) {
                 toastr.warning("Vui lòng đăng nhập để dislike bài viết");
                 return false;
             }
             DislikeService.DislikeAnswer($scope.DislikeData).then(function (result) {
-                console.log(result);
                 if (result.data.checkLikeAndDislike) {
                     $("#like-" + answerId).text(result.data.totalLike);
                     $("#dislike-" + answerId).text(result.data.totalDislike);
