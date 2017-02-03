0287383f6b33b6e6a6df25e69152c7b6669079ae
hoangphucvu <ngohungphuc95@gmail.com>
2017-02-03 14:41:51 +0700

    Todo fix chat function

--- Source/public/frontend/client/controllers/chat.controller.client.js
+++ Source/public/frontend/client/controllers/chat.controller.client.js
@@ -1,49 +1,54 @@
-(function() {
-	'use strict';
+(function () {
+    'use strict';
 
-	angular.module('ChatBotApp')
-		.controller('ChatController', ChatController);
+    angular.module('ChatBotApp')
+        .controller('ChatController', ChatController);
 
-	ChatController.$inject = ['$scope', 'localStorageService', '$document'];
+    ChatController.$inject = ['$scope', 'localStorageService', '$document'];
 
-	function ChatController($scope, localStorageService, $document) {
-		var socket = io.connect();
-		var loginUser = localStorageService.cookie.get('currentUser');
-		var facebookUser = localStorageService.cookie.get('facebookUser');
-		var currentUser = loginUser ? loginUser : facebookUser;
-		socket.emit('new user', currentUser);
-		var chat = $('.media-list');
-		var onlineList = $('.list-online');
-		var currentTime = new Date().toLocaleString();
-		$scope.SendMessage = function() {
-			if(!currentUser) {
-				toastr.warning("Vui lòng đăng nhập để tham gia phòng chat");
-				return false;
-			}
-			var message = $("#chatMessageSend").val();
-			if($.trim($("#chatMessageSend").val()) === '') {
-				toastr.warning("Vui lòng nhập tin nhắn");
-				return false;
-			}
-			var messageInfo = {
-				username: currentUser,
-				message: message
-			};
-			socket.emit('send message', messageInfo);
-			$("#chatMessageSend").val('');
-		};
+    function ChatController($scope, localStorageService, $document) {
+        var socket = io.connect();
+        var loginUser = localStorageService.cookie.get('currentUser');
+        var facebookUser = localStorageService.cookie.get('facebookUser');
+        var currentUser = loginUser ? loginUser : facebookUser;
+        var currentTime = new Date().toLocaleString();
+        socket.emit('new user', currentUser);
 
-		socket.on('new message', function(data) {
-			chat.append('<p><div class="media-body" id="chat-element">' + data.message + '<br><small class="text-muted">' + data.username + ' | ' + currentTime + '</small></div></p>');
-			var myDiv = document.getElementById('chat-element');
-			myDiv.scrollTop = myDiv.scrollHeight;
-		});
-		socket.on('get users', function(data) {
-			var listUserOnline = '';
-			for(var i = 0; i < data.length; i++) {
-				listUserOnline += '<p>' + data[i] + '</p>';
-			}
-			onlineList.html(listUserOnline);
-		});
-	}
+        var chat = $('.media-list');
+        var onlineList = $('.list-online');
+        $scope.onlineLists = [];
+
+        $scope.SendMessage = function () {
+            if (!currentUser) {
+                toastr.warning("Vui lòng đăng nhập để tham gia phòng chat");
+                return false;
+            }
+            var message = $("#chatMessageSend").val();
+            if ($.trim($("#chatMessageSend").val()) === '') {
+                toastr.warning("Vui lòng nhập tin nhắn");
+                return false;
+            }
+            var messageInfo = {
+                username: currentUser,
+                message: message,
+                currentTime: currentTime
+            };
+            socket.emit('send message', messageInfo);
+            $("#chatMessageSend").val('');
+        };
+
+        socket.on('new message', function (data) {
+            chat.append('<p><div class="media-body" id="chat-element">' + data.message + '<br><small class="text-muted">' + data.username + ' | ' + currentTime + '</small></div></p>');
+            var myDiv = document.getElementById('chat-element');
+            myDiv.scrollTop = myDiv.scrollHeight;
+        });
+
+        socket.on('get users', function (data) {
+            var listUserOnline = '';
+            for (var i = 0; i < data.length; i++) {
+                listUserOnline += '<p>' + data[i] + '</p>';
+            }
+            onlineList.html(listUserOnline);
+        });
+    }
 })();
